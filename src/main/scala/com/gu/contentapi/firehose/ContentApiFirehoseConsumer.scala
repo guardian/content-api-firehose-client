36cd72473c96f5a8495de89afb7652ba12fa95ed
Andy Gallagher <andy.gallagher@theguardian.com>
2017-10-13 10:40:17 +0100

    adding support for atom updates

--- src/main/scala/com/gu/contentapi/firehose/ContentApiFirehoseConsumer.scala
+++ src/main/scala/com/gu/contentapi/firehose/ContentApiFirehoseConsumer.scala
@@ -1,12 +1,12 @@
 package com.gu.contentapi.firehose
 
-import com.amazonaws.services.kinesis.clientlibrary.interfaces.{ IRecordProcessor, IRecordProcessorFactory }
+import com.amazonaws.services.kinesis.clientlibrary.interfaces.{IRecordProcessor, IRecordProcessorFactory}
 import com.gu.contentapi.client.model.v1.Content
 import com.gu.contentapi.firehose.client.StreamListener
-import com.gu.contentapi.firehose.kinesis.{ KinesisStreamReader, KinesisStreamReaderConfig, SingleEventProcessor }
-import com.gu.crier.model.event.v1.EventPayload.UnknownUnionField
+import com.gu.contentapi.firehose.kinesis.{KinesisStreamReader, KinesisStreamReaderConfig, SingleEventProcessor}
+import com.gu.crier.model.event.v1.EventPayload.{Atom, UnknownUnionField}
 import com.gu.crier.model.event.v1.EventType.EnumUnknownEventType
-import com.gu.crier.model.event.v1.{ Event, EventPayload, EventType }
+import com.gu.crier.model.event.v1.{Event, EventPayload, EventType, ItemType}
 
 import scala.concurrent.duration._
 
@@ -29,20 +29,18 @@ class ContentApiEventProcessor(filterProductionMonitoring: Boolean, override val
     event.eventType match {
 
       case EventType.Update | EventType.RetrievableUpdate =>
-        event.payload.foreach { payload =>
-          payload match {
+        event.payload.foreach {
             case EventPayload.Content(content) => streamListener.contentUpdate(content)
             case EventPayload.RetrievableContent(content) => streamListener.contentRetrievableUpdate(content)
-            case EventPayload.Atom(atom) => /* do nothing yet - TODO update */
+            case EventPayload.Atom(atom) => streamListener.atomUpdate(atom)
             case UnknownUnionField(e) => logger.warn(s"Received an unknown event payload $e. You should possibly consider updating")
-          }
         }
 
       case EventType.Delete =>
         if (filterProductionMonitoring && event.payloadId.startsWith("production-monitoring")) {
           // do nothing.
         } else {
-          /* TODO handle atom deletion here */
+          // Atom delete events are currently not supported by CAPI and therefore Crier
           streamListener.contentTakedown(event.payloadId)
         }
 
