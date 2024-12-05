d595ef98b726d0c2af1c4e2c7199325d3b39e035
Roberto Tyley <52038+rtyley@users.noreply.github.com>
2024-12-05 11:11:32 +0000

    Introduce tests: Add test for AWS JSON protocol conflicts
    This change is prompted by issues in other projects that _use_ the
    content-api-firehose-client - in the `apple-news` project, updating another
    library transitively updated the `aws-json-protocol` to a version that
    was incompatible with the AWS SDK artifacts bought in by
    content-api-firehose-client - leading to runtime errors:
    
    https://github.com/guardian/apple-news/pull/386#issuecomment-2501369378
    
    Here we introduce an integration test that exercises the AWS SDK
    `DescribeStreamSummary` call that failed - it checks that the response
    can be successfully processed by the AWS SDK. Note that although we have
    introduced this test, and it _passes_, this doesn't mean we've fixed the
    issue with other projects yet - it just allows us to reproduce the issue,
    by bumping the version on `aws-json-protocol` (ie by adding
    `"software.amazon.awssdk" % "aws-json-protocol" % "2.29.23"` to the deps),
    which we do outside of this branch.
    
    In order to get the test running in CI, we have to allow the GitHub Actions
    workflow to make the AWS SDK `DescribeStreamSummary` call, which requires
    AWS credentials.
    
    We need a IAM role that we can use in the GitHub Actions runners to
    have access to the kinesis stream we’re describing - this is the same
    approach taken in `facia-scala-client` with
    https://github.com/guardian/facia-scala-client/pull/272 .
    
    This commit copies the example cdk stack from facia-scala-client,
    updating the version of (gu)cdk used to the latest version, and tweaks
    the role policies to match what we need to do.
    
    This includes a fix from Akash to get Region and Account - they were `undefined`
    before! As Akash says:
    
    > scope here is of type App. The region and account properties of scope will not be meaningful here as we're not using CDK's Stage construct. That is, they'll return undefined.
    
    https://github.com/guardian/content-api-firehose-client/pull/51#discussion_r1870959622

--- src/main/scala/com/gu/contentapi/firehose/kinesis/KinesisStreamReader.scala
+++ src/main/scala/com/gu/contentapi/firehose/kinesis/KinesisStreamReader.scala
@@ -1,16 +1,35 @@
 package com.gu.contentapi.firehose.kinesis
 
-import java.util.concurrent.Executors
 import com.google.common.util.concurrent.ThreadFactoryBuilder
+import com.gu.contentapi.firehose.kinesis.KinesisStreamReader.{ checkApiAccess, kinesisClientFor }
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
-import java.util.UUID
-import software.amazon.kinesis.common.{ ConfigsBuilder, InitialPositionInStream, KinesisClientUtil }
-import software.amazon.kinesis.coordinator.Scheduler
-import software.amazon.kinesis.processor.ShardRecordProcessorFactory
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.cloudwatch.CloudWatchAsyncClient
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
 import software.amazon.awssdk.services.kinesis.KinesisAsyncClient
+import software.amazon.kinesis.common.KinesisClientUtil.createKinesisAsyncClient
+import software.amazon.kinesis.common.{ ConfigsBuilder, InitialPositionInStream, KinesisRequestsBuilder }
+import software.amazon.kinesis.coordinator.Scheduler
+import software.amazon.kinesis.processor.ShardRecordProcessorFactory
+
+import java.util.UUID
+import java.util.concurrent.Executors
+
+object KinesisStreamReader {
+  def kinesisClientFor(credentialsProvider: AwsCredentialsProvider, region: Region): KinesisAsyncClient =
+    createKinesisAsyncClient(
+      KinesisAsyncClient.builder().credentialsProvider(credentialsProvider).region(region))
+
+  /**
+   * This method is a quick check to ensure that the running JVM process can execute a successful AWS SDK API call
+   * to Kinesis. It blocks and fails fast. See https://github.com/guardian/content-api-firehose-client/pull/51
+   */
+  def checkApiAccess(client: KinesisAsyncClient, streamName: String): Unit = {
+    val response = client.describeStreamSummary(
+      KinesisRequestsBuilder.describeStreamSummaryRequestBuilder.streamName(streamName).build).get()
+    require(response.streamDescriptionSummary().streamName() == streamName)
+  }
+}
 
 trait KinesisStreamReader {
   val credentialsProvider: AwsCredentialsProvider
@@ -23,10 +42,7 @@ trait KinesisStreamReader {
   /* Unique ID for the worker thread */
   private val workerId = UUID.randomUUID().toString
 
-  private val kinesisClient = KinesisClientUtil.createKinesisAsyncClient(
-    KinesisAsyncClient.builder()
-      .credentialsProvider(credentialsProvider)
-      .region(Region.of(kinesisStreamReaderConfig.awsRegion)))
+  private val kinesisClient = kinesisClientFor(credentialsProvider, Region.of(kinesisStreamReaderConfig.awsRegion))
 
   private val dynamoClient = DynamoDbAsyncClient.builder().credentialsProvider(credentialsProvider).region(Region.of(kinesisStreamReaderConfig.awsRegion)).build()
   private val cwClient = CloudWatchAsyncClient.builder().credentialsProvider(credentialsProvider).region(Region.of(kinesisStreamReaderConfig.awsRegion)).build()
@@ -56,6 +72,7 @@ trait KinesisStreamReader {
 
   def start(): Thread = {
     workerThread.start()
+    checkApiAccess(kinesisClient, kinesisStreamReaderConfig.streamName)
     workerThread
   }
 
